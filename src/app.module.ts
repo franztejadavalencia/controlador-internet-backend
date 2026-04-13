ad2dc0bf3271367634049539019c1c5356bbf442
Franz Eduardo Tejada Valencia <franztejada@hotmail.com>
2026-04-13 19:30:23 +0000

    Infraestructura inicial y conexión db

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'; 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NetworkService } from './network/network.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, NetworkService],
+})
+export class AppModule {}
---
+++ src/network/network.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { exec } from 'child_process';
+
+@Injectable()
+export class NetworkService implements OnModuleInit {
+  onModuleInit() {
+    console.log('Configurando reglase de red (NAT/Forwarding)...');
+    const natCommand = 'sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE';
+    const forwardCommand = 'sudo iptables -A FORWARD -j ACCEPT';
+    exec(`${natCommand} && ${forwardCommand}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al configurar red: ${error.message}`);
+        return;
+      }
+      console.log('Red configurada con correctamente.');
+    });
+  }
+}
